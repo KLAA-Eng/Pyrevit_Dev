a0ceda25e3ab4cb9021c2d798f5ab0065c0b7dbc
RBKLAA <rbailie@klaa.com>
2026-03-24 14:02:44 +0000

    update code

--- KL&A Tools.tab/KL&A Tools.panel/Launch Graphic Standards.pushbutton/script.py
+++ KL&A Tools.tab/KL&A Tools.panel/Launch Graphic Standards.pushbutton/script.py
@@ -4,8 +4,4 @@ __author__ = "Your Name"
 
 from pyrevit import forms
 
-<<<<<<< HEAD
 forms.alert("Hello World, modify your code as you see fit!", title="Hello World", ok=True)
-=======
-forms.alert("Hello World", title="Hello World", ok=True)git up+
->>>>>>> 51edf02 (resolve conflict)
