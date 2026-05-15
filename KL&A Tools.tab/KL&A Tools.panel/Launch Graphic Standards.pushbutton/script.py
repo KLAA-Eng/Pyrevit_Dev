c275e68e242631ee29bf17c29a55f531d8ad6cff
Ron Bailie <rbailie@klaa.com>
2026-05-15 10:49:01 -0600

    lean version test

--- KL&A Tools.tab/KL&A Tools.panel/Launch Graphic Standards.pushbutton/script.py
+++ KL&A Tools.tab/KL&A Tools.panel/Launch Graphic Standards.pushbutton/script.py
@@ -1,53 +1,62 @@
-# -*- coding: utf-8 -*-
-__title__ = "Open Graphics Standards Bluebeam Session"
-__doc__ = "Launches KL&A Graphics Standards Bluebeam Studio Session from Revit."
+# # -*- coding: utf-8 -*-
+# __title__ = "Open Graphics Standards Bluebeam Session"
+# __doc__ = "Launches KL&A Graphics Standards Bluebeam Studio Session from Revit."
 
-import re
+# import re
+# import subprocess
+
+# from pyrevit import forms, script
+
+# logger = script.get_logger()
+
+# SESSION_ID = "665-447-452"
+
+# def normalize_session_id(raw_value):
+#     if not raw_value:
+#         return None
+
+#     raw_value = str(raw_value).strip()
+#     match = re.search(r'(\d{3}-\d{3}-\d{3}|\d{9})', raw_value)
+
+#     if not match:
+#         return None
+
+#     digits = re.sub(r'\D', '', match.group(1))
+
+#     if len(digits) != 9:
+#         return None
+
+#     return "{}-{}-{}".format(digits[0:3], digits[3:6], digits[6:9])
+
+# session_id = normalize_session_id(SESSION_ID)
+
+# if not session_id:
+#     forms.alert(
+#         "The SESSION_ID is not valid.",
+#         title="Invalid Bluebeam Session ID",
+#         warn_icon=True
+#     )
+#     script.exit()
+
+# studio_url = "studio://studio.bluebeam.com/{}/".format(session_id)
+# logger.info("Launching Bluebeam Studio Session: {}".format(studio_url))
+
+# try:
+#     subprocess.Popen(["cmd", "/c", "start", "", studio_url], shell=False)
+# except Exception as ex:
+#     forms.alert(
+#         "Could not launch Session."
+#         .format(str(ex)),
+#         title="Launch Failed",
+#         warn_icon=True
+#     )
+#     script.exit()
+
+# -*- coding: utf-8 -*-
+__title__ = "Open\n KL&A Graphics Standards Bluebeam Session"
+
 import subprocess
 
-from pyrevit import forms, script
+STUDIO_URL = "studio://studio.bluebeam.com/665-447-452/"
 
-logger = script.get_logger()
-
-SESSION_ID = "665-447-452"
-
-def normalize_session_id(raw_value):
-    if not raw_value:
-        return None
-
-    raw_value = str(raw_value).strip()
-    match = re.search(r'(\d{3}-\d{3}-\d{3}|\d{9})', raw_value)
-
-    if not match:
-        return None
-
-    digits = re.sub(r'\D', '', match.group(1))
-
-    if len(digits) != 9:
-        return None
-
-    return "{}-{}-{}".format(digits[0:3], digits[3:6], digits[6:9])
-
-session_id = normalize_session_id(SESSION_ID)
-
-if not session_id:
-    forms.alert(
-        "The SESSION_ID is not valid.",
-        title="Invalid Bluebeam Session ID",
-        warn_icon=True
-    )
-    script.exit()
-
-studio_url = "studio://studio.bluebeam.com/{}/".format(session_id)
-logger.info("Launching Bluebeam Studio Session: {}".format(studio_url))
-
-try:
-    subprocess.Popen(["cmd", "/c", "start", "", studio_url], shell=False)
-except Exception as ex:
-    forms.alert(
-        "Could not launch Session."
-        .format(str(ex)),
-        title="Launch Failed",
-        warn_icon=True
-    )
-    script.exit()
+subprocess.Popen(["cmd", "/c", "start", "", STUDIO_URL], shell=False)
