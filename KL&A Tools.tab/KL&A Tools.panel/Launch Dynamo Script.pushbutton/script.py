68260b20459dc8841a4a5099cc7070587dc162c0
Ron Bailie <rbailie@klaa.com>
2026-05-15 12:36:44 -0600

    resolve runtime errors

--- KL&A Tools.tab/KL&A Tools.panel/Launch Dynamo Script.pushbutton/script.py
+++ KL&A Tools.tab/KL&A Tools.panel/Launch Dynamo Script.pushbutton/script.py
@@ -1,28 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-
 import os
-import sys
 import clr
 
-from pyrevit import script, forms
+from pyrevit import forms, script, HOST_APP, UI
 
 logger = script.get_logger()
+bundle_dir = os.path.dirname(__file__)
+dyn_file = os.path.join(bundle_dir, "MyGraph.dyn")
 
-# Get the current pushbutton folder
-BUNDLE_DIR = os.path.dirname(__file__)
-DYN_FILE = os.path.join(BUNDLE_DIR, "script.dyn")
+if not os.path.exists(dyn_file):
+    forms.alert("Graph not found:\n{}".format(dyn_file), exitscript=True)
 
-if not os.path.exists(DYN_FILE):
-    forms.alert("Dynamo graph not found:\n{}".format(DYN_FILE), exitscript=True)
-
-try:
-    # pyRevit runtime types
+def run_graph(graph_path):
     clr.AddReference('PyRevitLoader')
     from pyrevit.runtime.types import DynamoBIMEngine, ScriptData, ScriptRuntime, ScriptRuntimeConfigs
 
     scriptdata = ScriptData()
-    scriptdata.ScriptPath = DYN_FILE
+    scriptdata.ScriptPath = graph_path
 
     config = ScriptRuntimeConfigs()
     runtime = ScriptRuntime(scriptdata, config)
@@ -30,6 +25,22 @@ try:
     engine = DynamoBIMEngine()
     engine.Execute(runtime)
 
+try:
+    run_graph(dyn_file)
+
 except Exception as ex:
-    logger.error("Failed to execute Dynamo graph: {}".format(ex))
-    forms.alert("Failed to execute Dynamo graph.\n\n{}".format(ex), exitscript=True)
+    msg = str(ex)
+    logger.error(msg)
+
+    forms.alert(
+        "Dynamo failed while initializing inside Revit.\n\n"
+        "This usually means Dynamo itself cannot start cleanly in this session,\n"
+        "not that the graph path is wrong.\n\n"
+        "Try:\n"
+        "1. Open Dynamo manually in Revit\n"
+        "2. Disable Dynamo packages/custom nodes\n"
+        "3. Update/Reboot Revit\n"
+        "4. Test again\n\n"
+        "Error:\n{}".format(msg),
+        exitscript=True
+    )
