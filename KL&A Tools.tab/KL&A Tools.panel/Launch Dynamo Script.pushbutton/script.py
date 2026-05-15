66a439ca68eb6f462743791fb0002dce1bd4b267
Ron Bailie <rbailie@klaa.com>
2026-05-15 12:51:12 -0600

    use ActiveUIDocument to access open file

--- KL&A Tools.tab/KL&A Tools.panel/Launch Dynamo Script.pushbutton/script.py
+++ KL&A Tools.tab/KL&A Tools.panel/Launch Dynamo Script.pushbutton/script.py
@@ -2,8 +2,8 @@
 import os
 import clr
 
+from pyrevit import HOST_APP, forms
 from Autodesk.Revit.UI import Result
-from pyrevit import revit, forms
 
 bundle_dir = os.path.dirname(__file__)
 dyn_path = os.path.join(bundle_dir, 'script.dyn')
@@ -11,20 +11,20 @@ dyn_path = os.path.join(bundle_dir, 'script.dyn')
 if not os.path.exists(dyn_path):
     forms.alert('Could not find script.dyn:\n{}'.format(dyn_path), exitscript=True)
 
+uiapp = HOST_APP.uiapp
+uidoc = uiapp.ActiveUIDocument
+
+if uidoc is None or uidoc.Document is None:
+    forms.alert('Open a Revit project or family document first, then run the button.', exitscript=True)
+
 try:
     clr.AddReference('DynamoRevitDS')
 except:
-    try:
-        clr.AddReference('DynamoRevit')
-    except Exception as ex:
-        forms.alert('Could not load DynamoRevit assembly.\n\n{}'.format(ex), exitscript=True)
+    clr.AddReference('DynamoRevit')
 
 from Dynamo.Applications import DynamoRevit, DynamoRevitCommandData, JournalKeys
 
-cmd_data = DynamoRevitCommandData()
-cmd_data.Application = __revit__
-
-journal_data = {
+jrnl = {
     JournalKeys.ShowUiKey: 'False',
     JournalKeys.AutomationModeKey: 'True',
     JournalKeys.DynPathKey: dyn_path,
@@ -34,10 +34,12 @@ journal_data = {
     JournalKeys.ModelNodesInfo: 'False'
 }
 
-cmd_data.JournalData = journal_data
+cmd = DynamoRevitCommandData()
+cmd.Application = uiapp
+cmd.JournalData = jrnl
 
 dynamo = DynamoRevit()
-result = dynamo.ExecuteCommand(cmd_data)
+result = dynamo.ExecuteCommand(cmd)
 
 if result != Result.Succeeded:
-    forms.alert('Dynamo did not return Result.Succeeded.\nReturned: {}'.format(result))
+    forms.alert('Dynamo returned: {}'.format(result))
