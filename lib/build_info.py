a8c6c56c5decf61a9de1f81308b06803baf2bedb
MadKLAA <lmadden@klaa.com>
2026-08-19 17:19:13 -0600

    Bump version to 0.0.5.beta

--- lib/build_info.py
+++ lib/build_info.py
@@ -1,9 +1,9 @@
 # This file is generated from version.json.
 # Do not edit by hand; update version.json and regenerate instead.
 
-VERSION = "0.0.4.beta"
+VERSION = "0.0.5.beta"
 CHANNEL = "beta"
-RELEASE_DATE = "2026-08-17"
-GIT_TAG = "v0.0.4.beta"
-GIT_SHA = "5a0d8fb"
-BUILD_DATE = "2026-08-17"
+RELEASE_DATE = "2026-08-19"
+GIT_TAG = "v0.0.5.beta"
+GIT_SHA = "6b0dbb6"
+BUILD_DATE = "2026-08-19"
